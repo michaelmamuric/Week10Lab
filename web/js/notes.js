d8c72e9b6110403c55f18d22ea76a289e3631d5f
Michael Mamuric <michael.mamuric@edu.sait.ca>
2020-03-28 20:43:35 -0600

    Use Note object instead of passing JSON object to AJAX response

--- web/js/notes.js
+++ web/js/notes.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     // Before Edit
-    $(".beforeEdit").submit(function(event) {
+    $(".beforeEdit").submit(function(e) {
         $.ajax({
             type: "post",
             url: $(this).attr("action"),
@@ -31,6 +31,6 @@ $(document).ready(function() {
                 $("body").html("An error has occured.");
             }
         });
-        event.preventDefault();
+        e.preventDefault();
     });
 });
